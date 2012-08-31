387096bdc2845e88e8155c219d1d60dfa8cbcdec
Renaud Tircher <rtircher@thoughtworks.com>
2012-08-30 21:02:44 -0700

    Removing dependency on clojure 1.4 which conflicts with noir and cljsbuild

--- project.clj
+++ project.clj
@@ -1,14 +1,17 @@
 (defproject iTircher "0.1.0"
   :description "Personal website written to test random stuffs with Clojure, ClojureScript,..."
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [borneo "0.3.0"]
-                 [noir "1.2.1"]
-                 [enlive "1.0.0"]]
+                 [noir "1.2.2"]
+                 [enlive "1.0.1"]]
   :dev-dependencies [[speclj "2.1.2"]
                      [speclj-growl "1.0.1"]]
   :test-path "spec/"
-  :plugins [[lein-cljsbuild "0.1.8"]]
+  :plugins [[lein-cljsbuild "0.1.8"]] ;; Update to latest when
+  ;; possible but currently generates an error using clojure 1.4
+  ;; (noir depends on clojure 1.3 which conflicts with
+  ;; cljsbuilds > 0.1.8 that depends on clojure 1.4)
   :hooks [leiningen.cljsbuild]
   :main iTircher.server
   :cljsbuild {
