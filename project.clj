c59445657f53887419081e461895a64beaff850b
Renaud Tircher <renaud.tircher@gmail.com>
2013-03-26 20:25:58 +0800

    Using lein2 profiles and updating to latest cljsbuild plugin

--- project.clj
+++ project.clj
@@ -6,38 +6,35 @@
                  [compojure "1.1.5"]
                  [lib-noir "0.4.6"]
                  [enlive "1.1.1"]]
-  :dev-dependencies [[speclj "2.1.2"]
-                     [speclj-growl "1.0.1"]
-                     ; [[ring-mock "0.1.2"]]
-                     ]
-  :test-path "spec/"
-  :plugins [[lein-cljsbuild "0.1.8"] ;; Update to latest when
-  ;; possible but currently generates an error using clojure 1.4
-  ;; (noir depends on clojure 1.3 which conflicts with
-  ;; cljsbuilds > 0.1.8 that depends on clojure 1.4)
-            [lein-haml-sass "0.2.5"]
-            [lein-ring "0.8.3"]]
+
+  :profiles {:dev {:dependencies [[speclj "2.1.2"]
+                                  ;; [[ring-mock "0.1.2"]]
+                                  [speclj-growl "1.0.1"]]
+                   :plugins [[lein-cljsbuild "0.3.0"]
+                             [lein-haml-sass "0.2.5"]
+                             [lein-ring "0.8.3"]
+                             [speclj "2.5.0"]]
+                  :test-path "spec/"
+                  }}
 
   :ring {:handler iPerture.core/app
          :port 5000}
 
-  :cljsbuild {
-    :builds [{
-    ; The path to the top-level ClojureScript source directory:
-    :source-path "src/iPerture/cljs"
-    :compiler {
-      :output-to "resources/public/js/main.js"
-      ; The optimization level.  May be :whitespace, :simple, or :advanced.
-      ; Defaults to :whitespace.
-      :optimizations :whitespace
-      ;; :externs ["externs/jquery.js"]
-      :pretty-print true}}]}
+  :cljsbuild {:builds [{
+                ;; The path to the top-level ClojureScript source directory:
+                :source-paths ["src/iPerture/cljs"],
+                :compiler
+                {:pretty-print true
+                 :output-to "resources/public/js/main.js"
+                 ;; :externs ["externs/jquery.js"]
+                 ;; The optimization level.  May be :whitespace, :simple, or :advanced
+                 ;; Defaults to :whitespace
+                 :optimizations :whitespace}}]}
 
   :haml {:src "src/iPerture/views/haml/"
          :output-directory "resources/public/html"
          :output-extension "html"
-         :gem-version "4.0.0"
-         }
+         :gem-version "4.0.0"}
 
   :sass {:src "src/iPerture/views/sass/"
          :output-directory "resources/public/css"
