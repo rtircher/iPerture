6c71e5c433c3fa6eb1f207d88f660bf97f8fb230
Renaud Tircher <renaud.tircher@gmail.com>
2013-04-06 12:31:08 +0800

    Extracting out should-have-been-called[-with] funtions

--- spec/iPerture/albums/albums_controller_spec.clj
+++ spec/iPerture/albums/albums_controller_spec.clj
@@ -16,36 +16,42 @@
 (defn match-selector [selector enlive-html]
   (should (select selector enlive-html)))
 
+(defmacro should-have-been-called [function body]
+  `(let [function-has-been-called?# (atom false)]
+     (with-redefs [~function
+                   (fn [& _#] (reset! function-has-been-called?# true))]
+       ~body
+       (should @function-has-been-called?#))))
+
+(defmacro should-have-been-called-with [function expected-params body]
+  `(let [params-received# (atom nil)]
+     (with-redefs [~function
+                   (fn [& params#] (reset! params-received# params#))]
+       ~body
+       (should= ~expected-params @params-received#))))
+
 (describe "albums-controller"
 
   (describe "fn new"
     (it "should call the render new album view function"
-      (def have-called-render-new-album (atom false))
-      (with-redefs [view/render-new-album
-                    (fn [& error-message] (reset! have-called-render-new-album true))]
-        (controller/new)
-        (should @have-called-render-new-album))))
+      (should-have-been-called view/render-new-album
+                               (controller/new))))
 
   (describe "create"
-    (context "when the album title is empty"
-      (it "should stay on the new page"
-        (def have-called-render-new-album (atom false))
-        (with-redefs [view/render-new-album
-                      (fn [& error-message] (reset! have-called-render-new-album true))]
-          (controller/create [""])
-          (should @have-called-render-new-album)))
-
-      (it "should ask the view to display an error message"
-        (def message-received (atom nil))
-        (with-redefs [view/render-new-album
-                      (fn [error-message] (reset! message-received error-message))]
-          (controller/create [""])
-          (should= "Please enter an album title" @message-received)))
-
-      (it "should not create a new album"
-        ))
-
     (it "should create a new album with the provided title"
       )
 
-    (it "should render an empty edit album page with the provided title")))
+    (it "should render an empty edit album page with the provided title")
+
+    (context "when the album title is empty"
+      (it "should stay on the new page"
+        (should-have-been-called view/render-new-album
+                                           (controller/create [""])))
+
+      (it "should ask the view to display an error message"
+        (should-have-been-called-with view/render-new-album
+                                      ["Please enter an album title"]
+                                      (controller/create [""])))
+
+      (it "should not create a new album"
+        ))))
