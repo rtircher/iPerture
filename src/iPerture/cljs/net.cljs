3079872f94f15bce6556b53974344c9abd98084a
Renaud Tircher <renaud.tircher@gmail.com>
2013-04-21 09:37:58 +0800

    Listening to all event exposed to closure XhrIo

--- src/iPerture/cljs/net.cljs
+++ src/iPerture/cljs/net.cljs
@@ -1,14 +1,16 @@
 (ns net
-  (:require [goog.net.XhrIo]
-            [goog.net.XhrManager]))
+  (:require goog.net.XhrIo
+            goog.net.XhrManager
+            goog.events.listen
+            [goog.net.EventType :as event-types]))
 
 (def headers nil)
 (def min-count 1)
 (def max-count 10)
 
 (def ^:private xhr-pool (goog.net.XhrIoPool. headers
-                                                min-count
-                                                max-count))
+                                             min-count
+                                             max-count))
 
 (def ^:private default-interval (* 30 1000))
 (def ^:private default-priority 100)
@@ -16,9 +18,41 @@
 (defn- get-xhr [callback priority]
   (.getObject xhr-pool callback (or priority default-priority)))
 
-(defn- send-request [method uri {:keys [content headers timeout on-success on-error]} xhr]
+(defn- get-response [xhr]
+  (let [xml  (try (.getResponseXml xhr) (catch js/Error e))
+        json (try (.getResponseJson xhr) (catch js/Error e))
+        text (.getResponseText xhr)]
+    (or xml json text)))
+
+(defn- listen-to-event [event-type xhr callback]
+  (when callback
+    (goog.events.listen xhr
+                        event-type
+                        (fn [event]
+                          (let [xhr    (aget event "target")
+                                data   (get-response xhr)
+                                status (.getStatus xhr)]
+                            (callback data status xhr))))))
+
+(defn- listen-to-all-events [xhr events-callbacks]
+  (doseq [[event-type callback] events-callbacks]
+    (listen-to-event event-type xhr callback)))
+
+(defn- map-callback-to-event-type [{:keys [on-ready-state-change on-complete on-success on-error on-ready on-timeout on-abort]}]
+  {event-types/READY_STATE_CHANGE on-ready-state-change
+   event-types/COMPLETE           on-complete
+   event-types/SUCCESS            on-success
+   event-types/ERROR              on-error
+   event-types/READY              on-ready
+   event-types/TIMEOUT            on-timeout
+   event-types/ABORT              on-abort})
+
+(defn- send-request [method uri {:keys [content headers timeout] :as options} xhr]
   (assert uri)
   (assert method)
+
+  (listen-to-all-events xhr (map-callback-to-event-type options))
+
   (.setTimeoutInterval xhr (or timeout default-interval))
   (.send xhr uri method content headers))
 
@@ -31,11 +65,18 @@ the following options can be provided:
 - content: the data to be sent with the request
 - headers: the headers to be sent with the request
 - timeout: number of milliseconds after which an incomplete request will be aborted.
+
+- on-ready-state-changed: function called to any browser readystatechange event
+- on-complete: function called when request has been resolved, whether successfully or not
 - on-success: function called when the request succeeds
 - on-error: function called when the request fails
+- on-ready: function called when an xhr request is ready to be used
+- on-timeout: function called when the request does not complete before the timeout interval has elapsed
+- on-abort: function called when the request is aborded
 "
   (get-xhr (partial send-request method uri options) priority))
 
+;; Helper functions for common REST operations
 (def get    (partial ajax "GET"))
 (def post   (partial ajax "POST"))
 (def put    (partial ajax "PUT"))
