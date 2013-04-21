189f2ea1470b5c56f74f5178dbf0566769ef22c2
Renaud Tircher <renaud.tircher@gmail.com>
2013-04-21 22:55:40 +0800

    Uploading photos using ajax form submit

--- src/iPerture/cljs/albums.cljs
+++ src/iPerture/cljs/albums.cljs
@@ -3,10 +3,15 @@
 
 (def $ (js* "$"))
 
+(defn- upload-photos [form]
+  (net/ajax-form form {:on-success (fn [& args] (js/console.log "FORM success: " args))
+                       :on-error (fn [& args] (js/console.log "FORM error: " args))}))
+
 ($ "ready"
    (fn []
-     (-> ($ "#edit-album")
-         (.on "submit" #(do
+     (-> ($ "#add-photo")
+         (.on "submit" #(let [form (aget % "target")
+                              uri  (aget form "action")]
                           (.preventDefault %)
-                          (js/console.log "Submitting form at url: " js/location.href)
-                          (net/put js/location.href))))))
+                          (js/console.log "Submitting form at url: " uri)
+                          (upload-photos form))))))
