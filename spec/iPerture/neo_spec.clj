16e0dcaae2afecfd00d0eadcecda89e03d6db35b
Renaud Tircher <renaud.tircher@gmail.com>
2013-04-25 10:56:26 +0800

    Adding function to add relationships

--- spec/iPerture/neo_spec.clj
+++ spec/iPerture/neo_spec.clj
@@ -26,4 +26,26 @@
         (with-redefs [borneo/traverse (fn
                                         ([_ _ _ _] [{:id "id"}])
                                         ([_ _] [{:id "rel-id"}]))]
-          (should= expected (@do-find "id" :type :rel1 :rel2)))))))
+          (should= expected (@do-find "id" :type :rel1 :rel2))))))
+
+  (describe "do-add-relationship!"
+    (with do-add-relationship! #'iPerture.neo/do-add-relationship!)
+
+    (it "should return nil if the parent was not found"
+      (with-redefs [borneo/traverse (fn [_ _ _ _] [])]
+        (should-be-nil (@do-add-relationship! "id" :parent-type {:id "child id"} :type))))
+
+    (context "when parent was found"
+      (with parent {:id "parent-id"})
+      (around [it]
+          (with-redefs [borneo/traverse (fn ([_ _ _ _] [@parent]))]
+            (it)))
+
+      (it "should ask neo to create a child of the parent"
+        (let [child {:id "child id"}]
+          (should-have-been-called-with borneo/create-child!
+                                        [@parent :type child]
+                                        (@do-add-relationship! "parent-id"
+                                                               :parent-type
+                                                               child
+                                                               :type)))))))
--- src/iPerture/neo.clj
+++ src/iPerture/neo.clj
@@ -6,17 +6,17 @@
   `(neo/with-local-db! (config :db-path)
      ~body))
 
-(defn create-child!
-  ([type props] (create-child! nil type props))
-  ([node type props]
-     (with-local-db!
-       (if node
-         (neo/create-child! node type props)
-         (neo/create-child! type props)))
-     props))
+(defn- do-create-child! [node type props]
+  (if node
+    (neo/create-child! node type props)
+    (neo/create-child! type props))
+  props)
 
-(defn add-relationship! [node-id rel-type child]
-  )
+(defn create-child!
+  ([type props]
+      (create-child! nil type props))
+  ([node type props]
+     (with-local-db! (do-create-child! node type props))))
 
 (defn- find-node [id type]
   (when-let [data (first (neo/traverse (neo/root)
@@ -24,6 +24,13 @@
                                        {:id id}
                                        type))]
     data))
+
+(defn- do-add-relationship! [parent-id parent-type child rel-type]
+  (when-let [parent (find-node parent-id parent-type)]
+    (neo/create-child! parent rel-type child)))
+
+(defn add-relationship! [parent-id parent-type child rel-type]
+  (with-local-db! (do-add-relationship! parent-id parent-type child rel-type)))
 
 (defn- find-rels [node relationship]
   (when-let [children (neo/traverse node relationship)]
