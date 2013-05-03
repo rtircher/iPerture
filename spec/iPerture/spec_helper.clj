23b7d549c38b6fe78b98c2f1052b884f70382ee9
Renaud Tircher <renaud.tircher@gmail.com>
2013-05-03 10:53:32 +0800

    allowing to specify the return value of stubbed function when testing if it has been called

--- spec/iPerture/spec_helper.clj
+++ spec/iPerture/spec_helper.clj
@@ -19,39 +19,51 @@
                 (map html/text (select selector enlive-html))))))
 
 (defn should-not-contain
-    ([selector enlive-html]
+  ([selector enlive-html]
      (should= '() (select selector enlive-html)))
 
   ([content selector enlive-html]
      (let [to-compare (if (coll? content) content [content])]
        (should-not= to-compare
-                (map html/text (select selector enlive-html))))))
+                    (map html/text (select selector enlive-html))))))
 
 (defn match-selector [selector enlive-html]
   (should (not (empty? (select selector enlive-html)))))
 
-(defmacro has-been-called [function body test-fn error-message]
-  `(let [function-has-been-called?# (atom false)]
-     (with-redefs [~function
-                   (fn [& _#]
-                     (reset! function-has-been-called?# true)
-                     (fn [& _#] nil))]
-       ~body
-       (if-not (~test-fn @function-has-been-called?#)
-         (throw (SpecFailure. (str ~error-message)))))))
+(defmacro has-been-called
+  ([function body test-fn error-message]
+     `(has-been-called ~function ~body ~test-fn ~error-message {}))
+  ([function body test-fn error-message {:keys [and-return]}]
+     `(let [function-has-been-called?# (atom false)]
+        (with-redefs [~function
+                      (fn [& _#]
+                        (reset! function-has-been-called?# true)
+                        (or ~and-return (fn [& _#] nil)))]
+          ~body
+          (if-not (~test-fn @function-has-been-called?#)
+            (throw (SpecFailure. (str ~error-message))))))))
 
-(defmacro should-have-been-called [function body]
-  `(has-been-called ~function ~body identity "Expected function to have been called"))
+(defmacro should-have-been-called
+  ([function body]
+     `(should-have-been-called ~function ~body {}))
+  ([function body opts]
+     `(has-been-called ~function ~body identity "Expected function to have been called" ~opts)))
 
-(defmacro should-not-have-been-called [function body]
-  `(has-been-called ~function ~body not "Expected function not to have been called"))
+(defmacro should-not-have-been-called
+  ([function body]
+     `(should-not-have-been-called ~function ~body {}))
+  ([function body opts]
+     `(has-been-called ~function ~body not "Expected function not to have been called" ~opts)))
 
-(defmacro should-have-been-called-with [function expected-params body]
-  `(let [params-received# (atom nil)]
-     ;; First check if the function was called at all
-     (should-have-been-called ~function ~body)
-     (with-redefs [~function
-                   (fn [& params#] (reset! params-received# params#)
-                     (fn [& _#] nil))]
-       ~body
-       (should= ~expected-params @params-received#))))
+(defmacro should-have-been-called-with
+  ([function expected-params body]
+     `(should-have-been-called-with ~function ~expected-params ~body {}))
+  ([function expected-params body {:keys [and-return] :as opts}]
+     `(let [params-received# (atom nil)]
+        ;; First check if the function was called at all
+        (should-have-been-called ~function ~body ~opts)
+        (with-redefs [~function
+                      (fn [& params#] (reset! params-received# params#)
+                        (or ~and-return (fn [& _#] nil)))]
+          ~body
+          (should= ~expected-params @params-received#)))))
