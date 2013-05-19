3e17ca09041412e3f25ec2960643afb85ae745db
Renaud Tircher <renaud.tircher@gmail.com>
2013-05-19 12:55:15 +0800

    Using my redefined chef-leiningen chef module

--- chef/site-cookbooks/iPerture/attributes/default.rb
+++ chef/site-cookbooks/iPerture/attributes/default.rb
@@ -1,3 +1,7 @@
 default['java']['install_flavor'] = 'oracle'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 default['java']['jdk_version'] = 7
+
+default[:leiningen][:version] = "2.1.3"
+default[:leiningen][:user] = "vagrant"
+default[:leiningen][:group] = "vagrant"
--- chef/site-cookbooks/iPerture/metadata.rb
+++ chef/site-cookbooks/iPerture/metadata.rb
@@ -6,3 +6,4 @@ version          '0.1.0'
 
 depends "java"
 depends "nginx"
+depends "leiningen"
