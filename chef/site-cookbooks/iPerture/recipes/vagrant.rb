e1a88f689229392c94b488a4400d2cbc6d9f138a
Renaud Tircher <renaud.tircher@gmail.com>
2013-05-21 23:04:52 +0800

    Restarting lein ring server on every provisioning

--- chef/site-cookbooks/iPerture/recipes/vagrant.rb
+++ chef/site-cookbooks/iPerture/recipes/vagrant.rb
@@ -1,1 +1,17 @@
 include_recipe "iPerture"
+
+execute "lein-ring-server" do
+  cwd "/vagrant"
+  command <<-EOS
+kill `ps ax | grep [c]ompojure | awk '{print $1}'`
+for i in {0..30}; do
+  netstat -an | grep 5000 &> /dev/null
+  if [ $? == 0 ]; then
+    sleep 1
+  else
+    break
+  fi
+done
+LEIN_ROOT=1 lein ring server-headless &> log/development.log &
+EOS
+end
