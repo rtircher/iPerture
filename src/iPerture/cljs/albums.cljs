4b292e350dabc2bb88e1ad708e4d147510582cd2
Renaud Tircher <renaud.tircher@gmail.com>
2013-06-01 00:00:28 +0800

    Append the newly added photo on successful submission

--- src/iPerture/cljs/albums.cljs
+++ src/iPerture/cljs/albums.cljs
@@ -3,10 +3,22 @@
             [enfocus.core :as ef])
   (:require-macros [enfocus.macros :as em]))
 
-(def ^:private edit-album "public/html/edit-album.html")
+(def ^:private edit-album "/html/edit-album.html")
+
+(defn- background-photo [photo-url]
+  (str "background-image:url('" photo-url "')"))
+
+(em/defsnippet ^:private thumbnail-model edit-album [".photos > *:first-child"]
+  [thumbnail-url]
+  [:.photo] (em/set-attr :style (background-photo thumbnail-url)))
+
+(defn- upload-success-handler [data]
+  (js/console.log "FORM success: " data)
+  (em/at js/document [".photos > *:last-child"]
+         (em/before (thumbnail-model (aget data "thumbnail-url")))))
 
 (defn- upload-photos [form]
-  (net/ajax-form form {:on-success (fn [data] (js/console.log "FORM success: " data))
+  (net/ajax-form form {:on-success upload-success-handler
                        :on-error (fn [& args] (js/console.log "FORM error: " args))}))
 
 (defn- photo-added-handler [& args]
