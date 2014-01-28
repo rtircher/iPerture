615b6c2b4a373c5f529279c45b786358caf1108f
Renaud Tircher <renaud.tircher@gmail.com>
2014-01-27 18:00:13 -0800

    reduced repetition

--- scripts/add-local-url-to-hosts.sh
+++ scripts/add-local-url-to-hosts.sh
@@ -4,6 +4,8 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cat /etc/hosts | grep "192\.168\.33\.88\s*local\.iperture\.tircher\.com" &> /dev/null
 if [ $? != 0 ]; then
-  echo "Adding '192.168.33.88   local.iperture.tircher.com local.iPerture.tircher.com' to your /etc/hosts file"
-  sudo sh -c 'echo "192.168.33.88\tlocal.iperture.tircher.com local.iPerture.tircher.com" >> /etc/hosts'
+  HOSTS_LINE="192.168.33.88   local.iperture.tircher.com local.iPerture.tircher.com"
+
+  echo "Adding '$HOSTS_LINE' to your /etc/hosts file"
+  sudo sh -c "echo \"$HOSTS_LINE\" >> /etc/hosts"
 fi
